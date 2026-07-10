9d6714f5d04a3cc675ef1e22bf9b9c14eb1b3f28
imanuel-creator <imanuelvalentinosuprapto@gmail.com>
2026-07-10 07:01:46 +0700

    Update koneksi.php

--- php/config/koneksi.php
+++ php/config/koneksi.php
@@ -3,9 +3,9 @@
 <?php
 $host     = getenv('MYSQLHOST')     ?: 'localhost';
 $port     = getenv('MYSQLPORT')     ?: '3306';
-$dbname   = getenv('MYSQLDATABASE') ?: 'mydb';
+$dbname   = getenv('MYSQLDATABASE') ?: 'db_bimbingan_belajar';
 $user     = getenv('MYSQLUSER')     ?: 'root';
-$password = getenv('MYSQLPASSWORD') ?: '';
+$password = getenv('MYSQLPASSWORD') ?: 'noel123';
 
 $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
